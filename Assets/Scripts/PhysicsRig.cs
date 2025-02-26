45e3ed83eb7541413e69f42476510d5e79781739
PabloVinii <pvinicius006@hotmail.com>
2025-02-26 00:22:52 -0300

    fisicas nos objetos

--- Assets/Scripts/PhysicsRig.cs
+++ Assets/Scripts/PhysicsRig.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class PhysicsRig : MonoBehaviour
 {
-    public Transform playerHead;
     public Transform leftController;
     public Transform rightController;
 
-    public ConfigurableJoint headJoint;
     public ConfigurableJoint leftHandJoint;
     public ConfigurableJoint rightHandJoint;
 
     public CapsuleCollider bodyCollider;
 
-
-    public float bodyHeightMin = 0.5f;
-    public float bodyHeightMax = 2;
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyHeightMin, bodyHeightMax);
-        bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height / 2,
-            playerHead.localPosition.z);
-
         leftHandJoint.targetPosition = leftController.localPosition;
         leftHandJoint.targetRotation = leftController.localRotation;
 
